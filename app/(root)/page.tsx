fbadc791f744a6177f3bcb4491e7fcd1580472af
Yuvraj-Achrekar <yuvraj.achrekar@gmail.com>
2024-11-08 22:05:02 +0530

    added sanity live fetch extracting types in sanity fetching startup with ID latest next canary ppr rendering

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,7 @@
 import SearchForm from "@/components/SearchForm";
-import StartupCard from "@/components/StartupCard";
+import StartupCard, { StartupCardType } from "@/components/StartupCard";
 import { client } from "@/sanity/lib/client";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 
 export default async function Home({
@@ -9,9 +10,8 @@ export default async function Home({
 	searchParams: Promise<{ query?: string }>;
 }) {
 	const query = (await searchParams).query;
-
-	const posts = await client.fetch(STARTUPS_QUERY);
-	console.log(posts);
+	const params = { search: query || null };
+	const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
 
 	return (
 		<>
@@ -39,6 +39,7 @@ export default async function Home({
 					)}
 				</ul>
 			</section>
+			<SanityLive />
 		</>
 	);
 }
---
+++ app/(root)/startup/[id]/page.tsx
@@ -0,0 +1,17 @@
+import { client } from "@/sanity/lib/client";
+import { STARTUP_BY_ID_QUERY } from "@/sanity/lib/queries";
+import { Startup } from "@/sanity/types";
+import { notFound } from "next/navigation";
+import React from "react";
+
+export const experimental_ppr = true;
+
+const page = async ({ params }: { params: Promise<{ id: string }> }) => {
+	const id = (await params).id;
+	const post: Startup = await client.fetch(STARTUP_BY_ID_QUERY, { id });
+
+	if (!post) return notFound();
+	return <div>{post.title}</div>;
+};
+
+export default page;
